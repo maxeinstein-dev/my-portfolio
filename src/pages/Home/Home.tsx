14bbb9f412b0510a0a8161a0cfb26907c9e95e5f
MaxEinstein-DEV <maxsueleinstein@hotmail.com>
2024-06-04 12:35:24 -0300

    feat: creates Hero component UI

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import NavBar from "../../components/NavBar/NavBar"
+import Hero from "./sections/Hero/Hero"
+
+const Home = () => {
+
+    return (
+      <>
+      <NavBar></NavBar>
+        <Hero/>
+      </>
+    )
+  }
+  
+  export default Home
+  
